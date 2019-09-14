7fd506bc394604f8c198975892b6294fa3c395ef
lvachon <lvachon@gmail.com>
2019-09-14 15:00:35 -0400

    Chaning webcam to 250ms frametime and ramdisk destination (lag and sdcard health)

--- initWebcam.sh
+++ initWebcam.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting webcam..."
-raspistill -o /home/pi/RobotPi/html/frame.jpg -tl 2000 -t 0 -w 1024 -h 768
+raspistill -o /home/pi/RobotPi/html/ramdisk/frame.jpg -tl 250 -t 0 -w 1024 -h 768
 
