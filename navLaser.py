263a7b1eb08dda575382f1a5535ab71bc8fc04cb
Luc Vachon <lvachon@gmail.com>
2019-09-30 01:33:59 +0100

    It actually works now!

--- navLaser.py
+++ navLaser.py
@@ -8,7 +8,7 @@ import time
 
 width=640
 height=480
-maxY=225
+maxY=250
 contours = ()
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -18,52 +18,64 @@ laserOn = 0
 time.sleep(0.1)
 laserImage = np.array(())
 darkImage = np.array(())
+while camera.analog_gain <= 1:
+	time.sleep(0.1)
+# Now fix the values
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
+g = camera.awb_gains
+camera.awb_mode = 'off'
+camera.awb_gains = g
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	cleanImage = frame.array
+	cleanImage = frame.array.copy()
 	if laserOn>0:
-		print("laseron")
 		os.system('echo 1 > ./html/led')
+		time.sleep(0.1)
 		laserImage = frame.array[:,:,2]
 	else:
-		print("laseroff")
 		os.system('echo 0 > ./html/led')
+		time.sleep(0.1)
 		darkImage = frame.array[:,:,2]
-	if laserImage.size>0 and darkImage.size>0 and laserOn==0:
+	if laserImage.size>0 and darkImage.size>0 and laserOn==1:
 		sub = cv2.subtract(laserImage,darkImage)
 		blur = cv2.GaussianBlur(sub,(5,5),0)
 		blobs = cv2.inRange(blur,20,255)
-		mask = cv2.multiply(blobs,1)
+		#cv2.line(blobs,(width/3,0),(width/3,height),32)
+		#cv2.line(blobs,(2*width/3,0),(2*width/3,height),32)
+		mask = blobs.copy() #cv2.multiply(blobs,1)
 		contours,hierarchy = cv2.findContours(blobs, 1, 2)
 		lMaxY=0
 		cMaxY=0
 		rMaxY=0
 		for cnt in contours:
-			x,y,w,h = cv2.boundingRect(cnt)
-			if w<2 or h<2:
+			if cv2.contourArea(cnt)<4:
 				continue
-			if x<width/3 and y>lMaxY:
-				lMaxY=y
-			if x>width/3 and x<2*width/3 and y>cMaxY:
-				cMaxY=y
-			if x>2*width/3 and y>rMaxY:
-				rMaxY=y
+			hull = cv2.convexHull(cnt)
+			for point in hull:
+				x = point[0][0]
+				y = point[0][1]
+				cv2.drawContours(cleanImage, [cnt], 0, (0,255,0), 3)
+				if x<width/3 and y>lMaxY:
+					lMaxY=y
+				if x>width/3 and x<2*width/3 and y>cMaxY:
+					cMaxY=y
+				if x>2*width/3 and y>rMaxY:
+					rMaxY=y
 		cv2.line(cleanImage,(0,lMaxY),(width/3,lMaxY),(0,0,255),8)
 		cv2.line(cleanImage,(width/3,cMaxY),(2*width/3,cMaxY),(0,0,255),8)
 		cv2.line(cleanImage,(2*width/3,rMaxY),(width,rMaxY),(0,0,255),8)
 		if lMaxY<maxY and cMaxY<maxY and rMaxY<maxY:
 			os.system('cd html;./fwd.sh')
-		else:
-			if lMaxY<maxY and (rMaxY>maxY or cMaxY>maxY):
-				os.system('cd html;./left.sh')
-			else:
-				if rMaxY<maxY and (lMaxY>maxY or cMaxY>maxY):
-							os.system('cd html;./right.sh')
-				else:
-					if cMaxY>maxY:
-						os.system('cd html;./bwd.sh')
-		time.sleep(1)
-		cv2.imwrite('./html/ramdisk/robot.jpg',mask, [int(cv2.IMWRITE_JPEG_QUALITY), 25])
+		elif lMaxY<maxY and (rMaxY>maxY or cMaxY>maxY):
+			os.system('cd html;./left.sh')
+		elif rMaxY<maxY and (lMaxY>maxY or cMaxY>maxY):
+			os.system('cd html;./right.sh')
+		elif cMaxY>maxY or (lMaxY>maxY and rMaxY>maxY):
+			os.system('cd html;./bwd.sh;./left.sh;./left.sh;./left.sh')
+			
+		time.sleep(0.1)
+		cv2.imwrite('./html/ramdisk/robot.jpg',np.vstack( ( np.hstack((laserImage,darkImage)) , np.hstack((sub,mask)) ) ), [int(cv2.IMWRITE_JPEG_QUALITY), 25])
+		cv2.imwrite('./html/ramdisk/frame.jpg',cleanImage, [int(cv2.IMWRITE_JPEG_QUALITY), 25])
 	rawCapture.truncate(0)
-	cv2.imwrite('./html/ramdisk/frame.jpg',cleanImage, [int(cv2.IMWRITE_JPEG_QUALITY), 25])
 	laserOn = 1-laserOn
 
