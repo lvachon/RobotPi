c2aca10e7dccafce7a54fc3c4fe3331dd5c62d59
Luc Vachon <lvachon@gmail.com>
2021-03-25 03:02:22 +0000

    intial GPS nav

--- v2/blinky.py
+++ v2/blinky.py
@@ -3,11 +3,14 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import numpy as np
+import math
 import time
 import json
 import os
 import board
 import digitalio
+import FaBo9Axis_MPU9250
+from haversine import haversine, Unit
 from adafruit_vl53l0x import VL53L0X
 
 def awb():
@@ -24,9 +27,13 @@ def getFrame():
 	camera.capture_sequence([frame],'rgb',True,(64,48))
 	return frame
 def getGPS():
-	f = open('/home/pi/RobotPi/html/ramdisk/gpsdata')
-	gpsData = json.load(f)
-	f.close()
+	try:
+		f = open('/home/pi/RobotPi/html/ramdisk/gpsdata')
+		gpsData = json.load(f)
+		f.close()
+	except:
+		if(f):f.close()
+		gpsData = {'lat':42,'lon':-71}
 	return gpsData
 def ledOn():
 	led.value=True
@@ -224,8 +231,70 @@ def writeTelemetry():
 	f.close()
 	os.system("printf \"AT+SEND=0,"+str(len(jstring))+","+jstring+"\\r\\n\" > /dev/serial0")
 
+
+#18.221 62.345 48.369 90.085
+minX=18.221
+maxX=62.345
+minY=48.369
+maxY=90.085
+
+def compass():
+        global minX
+        global minY
+        global maxX
+        global maxY
+        mag = mpu9250.readMagnet()
+        if(mag['y']>maxY):maxY=mag['y']
+        if(mag['y']<minY):minY=mag['y']
+        if(mag['x']>maxX):maxX=mag['x']
+        if(mag['x']<minX):minX=mag['x']
+        if(minX==maxX or minY==maxY): return 0
+        x = 2*(mag['x']-minX)/(maxX-minX) - 1
+        y = 2*(mag['y']-minY)/(maxY-minY) - 1
+        hdn = 180.0 * math.atan2(x,y)/math.pi - 90
+        if(hdn<0):hdn+=360
+        return hdn
+
+def bearingToPoint(srcLat,srcLon,destLat,destLon):
+	srcLat*=math.pi/180.0
+	srcLon*=math.pi/180
+	destLat*=math.pi/180
+	destLon*=math.pi/180
+	x = math.cos(destLat)*math.sin(destLon-srcLon)
+	y = math.cos(srcLat)*math.sin(destLat)-math.sin(srcLat)*math.cos(destLat)*math.cos(destLon-srcLon)
+	b = 180 * math.atan2(x,y)/math.pi
+	if(b<0):b+=360
+	return b
+
+waypoints = [(-90,0)] #TO THE SOUTH POLE!
+currentWaypoint = 0
+def autopilot():
+	global waypoints
+	global currentWaypoint
+	gps = getGPS()
+	if(gps==False):
+		print("No GPS")
+		return "s"
+	heading = compass()
+	dist = haversine((gps['lat'],gps['lon']),waypoints[currentWaypoint],Unit.METERS)
+	status['dist']=dist
+	status['heading']=heading
+	if(dist<10):
+		if(currentWaypoint+1<len(waypoints)):
+			currentWaypoint+=1
+		else:
+			return "s"
+	else:
+		desiredBearing = bearingToPoint(gps['lat'],gps['lon'],waypoints[currentWaypoint][0],waypoints[currentWaypoint][1])
+		bearingDiff = desiredBearing - heading
+		if(bearingDiff>180):bearingDiff-=360
+		if(bearingDiff<-180):bearingDiff+=360
+		if(bearingDiff>30): return "fr"
+		if(bearingDiff<-30): return "fl"
+		return "f"
+
 print("Init params")
-settings = {'srcThresh':32,'backLimit':3, 'minDist':750,'mode':'UV'}
+settings = {'srcThresh':32,'backLimit':3, 'minDist':750,'navMode':'GPS'}
 status = {'backCount':0}
 tofL=0
 tofR=0
@@ -272,39 +341,42 @@ camera.resolution = (humanWidth, humanHeight)
 camera.framerate = 4
 awb()
 initTof()
+mpu9250 = FaBo9Axis_MPU9250.MPU9250()
 
 
 while True:
 	readSettings()
 	tof=getTof()
 	tofMoves = avoid(tof)
-
-	if(tofMoves!="f" and settings['mode']=="UV"):
+	seekMoves = ""
+	if(tofMoves!="f" and settings['navMode']=="UV"):
 		uv=makeUVMap()
 		seekMoves = seek(uv)
-	else:
-		seekMoves = ""
+	if(tofMoves=="f" and settings['navMode']=="GPS"):
+		seekMoves = autopilot()
 	moves = tofMoves
 	if(tofMoves!="f"):
 		status['phase']='avoid'
-		if(seekMoves=="f"):
-			status['phase']='found'
-			status['backCount']=0
-			moves = ""
+		if(settings['navMode']=="UV"):
+			if(seekMoves=="f"):
+				status['phase']='found'
+				status['backCount']=0
+				moves = ""
 	else:
 		status['phase']='travel'
 		if(len(seekMoves)):
 			status['phase']='seek'
 			moves=seekMoves
-	if(tofMoves=="r" and seekMoves=="l"):
+	if(tofMoves=="r" and seekMoves=="l" and settings['navMode']=="UV"):
 		status['phase']='t-left'
 		moves="l"
-	if(tofMoves=="l" and seekMoves=="r"):
-		status['phase']='t- right'
+	if(tofMoves=="l" and seekMoves=="r" and settings['navMode']=="UV"):
+		status['phase']='t-right'
 		moves="r"
 	status['moves']=moves
 	status['tof']=tof
 	executeMoves(moves)
+	print(settings)
 	print(status)
 	camera.capture_sequence(['../html/ramdisk/frame.jpg'],'jpeg',True,None,0,False,thumbnail=None)
 	writeTelemetry()
